4722f63aa2896ea696d400090bd7e442957ba7e3
alexey <alexm171002@mail.ru>
2022-08-13 18:49:12 +0300

    3 with corrections

--- L2_program3/Program.cs
+++ L2_program3/Program.cs
@@ -2,7 +2,7 @@
 int a = Convert.ToInt32(Console.ReadLine());
 if (a == 6 ^ a == 7) {
     Console.WriteLine("да");
-} else if (a > 1 & a < 6) {
+} else if (a = 1 ^ a = 2 ^ a = 3 ^ a = 4 ^ a = 5) {
     Console.WriteLine("нет");
 } else {
     Console.WriteLine("Не день недели"); 
