d9ef58756a2a41587b796a92fa817d8e1106fef7
alexey <alexm171002@mail.ru>
2022-08-20 00:19:08 +0300

    new names

---
+++ L4_(25)/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите 2 числа через энтер");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(a,b);
+Console.WriteLine(result);
---
+++ L4_(27)/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Впишите, пожалуйста, до 9 знаков");
+
+int a = Convert.ToInt32(Console.ReadLine()); // получаем данные
+int b = 1; //узнаем количество знаков
+int c = 1; // получаем параллельно максимальный делитель
+double d = 0; // будет использоваться при определении делителей
+int e = 2; // будет использоваться при введении числа, сумма цифр которого кратно 2
+while (a/c > 10) {
+    c = c * 10;
+    b=b+1;
+} //выяснили размер
+if (b%2 != 0) {
+    e = 3; //введеное число, сумма цифр которого не кратна
+}
+int result = a/c;
+Console.WriteLine(result);
+result = result + (a%10);
+Console.WriteLine(result);
+if (b == 3) {
+    result = result + (a/10%10);
+}
+    while (d != ((b-e)/2)) {
+        c = c/10;
+        d = d+1;
+        double delitel1 = Math.Pow(10,d);
+        double delitel2 = Math.Pow(10,d+1);
+        double delitel3 = c*10;
+        result = result + Convert.ToInt32(Math.Floor((a % delitel3 / c)));
+        result = result + Convert.ToInt32((Math.Floor(a % delitel2 / delitel1)));
+        Console.WriteLine(result);
+    }
+
+Console.WriteLine(result);
+
