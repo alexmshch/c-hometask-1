c63f59f39e614211d9611e8db42b182edaf14e15
alexey <alexm171002@mail.ru>
2022-08-20 00:18:43 +0300

    new names

--- L4_program1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Введите 2 числа через энтер");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-
-double result = Math.Pow(a,b);
-Console.WriteLine(result);
