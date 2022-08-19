cb0dfac3036013a554ff68f1dc285f0a0cff2eca
alexey <alexm171002@mail.ru>
2022-08-19 18:11:26 +0300

    program 2 is ready

---
+++ L3_program2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите, используя кнопку Энтер координаты 2 точек");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+double result = Math.Pow(
+    (Math.Pow(x2-x1,2) +
+    Math.Pow(y2-y1,2) +
+    Math.Pow(z2-z1,2)),
+    0.5);
+Console.WriteLine(result);
