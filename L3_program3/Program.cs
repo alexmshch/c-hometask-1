a9c9f7910a3d2b972a6f174d54ea06529a3bfaf0
alexey <alexm171002@mail.ru>
2022-08-19 18:31:18 +0300

    program 3 is ready

---
+++ L3_program3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целочисленное число до 9 знаков");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+
+while (b <= a) {
+    Console.Write(Math.Pow(b,3));
+    b = b+1;
+    if (b <= a) {
+        Console.Write(",");
+    }
+}
+Console.WriteLine();
