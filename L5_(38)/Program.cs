e0d556d66409f185a4ee55fdb33a48e212195442
alexey <alexm171002@mail.ru>
2022-08-25 12:11:05 +0300

    L5 number 3 is ready

---
+++ L5_(38)/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] array = new double[5];
+Random rnd = new Random();
+int a = 0;
+double min = 100;
+double max = 0;
+double sum = 0;
+while (a < array.Length) {
+    array[a] = rnd.Next(1,99);
+    if (array[a] < min) {
+        min = array[a];
+    }
+    if (array[a] > max) {
+        max = array[a];
+    }
+    Console.Write(array[a]);
+    a = a+1;
+    if (a < array.Length) {
+        Console.Write(",");
+    }
+}
+Console.WriteLine("");
+sum = min + max;
+Console.WriteLine("Сумма минимального и максимального элемента: " + sum);
