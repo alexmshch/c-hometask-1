a29290efa3596eb4a8eb76e508ef225c6f584cf3
alexey <alexm171002@mail.ru>
2022-08-25 11:14:28 +0300

    five_1 is ready

---
+++ L5_(34)/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int [10];
+Random rnd = new Random();
+int a = 0;
+int b = 0;
+while (a < array.Length) {
+    array[a] = rnd.Next(100, 999);
+    if (array[a] % 2 == 0) {
+        b = b+1;
+    }
+    Console.Write(array[a]);
+    a = a+1;
+    if (a < array.Length) {
+       Console.Write(",");
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Четных элементов: " + b);
