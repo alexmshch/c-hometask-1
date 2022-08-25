543a420e8e39efb21400f2094f3964364fd892cc
alexey <alexm171002@mail.ru>
2022-08-25 10:45:16 +0300

    L4 is ready

--- L4_(29)/Program.cs
+++ L4_(29)/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//какая интересная и потрясающая задача, отлично, что она была четко и доступно объяснена))00))
+//какая задача - такое и решение
+Console.WriteLine("Ща буит мясо");
+Console.WriteLine("Извините, не сегодня, сегодня только магия");
+
+int a = 0;
+int sizeofarray = 7;
+int[] array = new int[sizeofarray];
+Random rnd = new Random();
+while (a < sizeofarray) {
+    array[a] = rnd.Next(1, 9);
+    Console.WriteLine(array[a]);
+    a = a+1;
+}
