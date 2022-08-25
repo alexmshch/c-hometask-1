bd4e65116b7d3ee0fb7595a52ab5cc7a54a169a4
alexey <alexm171002@mail.ru>
2022-08-25 11:23:39 +0300

    L5 number 2 is ready

---
+++ L5_(36)/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[10];
+Random rnd = new Random();
+int a = 0;
+int sum = 0;
+while (a < array.Length) {
+    array[a] = rnd.Next(-100, 100);
+    if ((a+1)%2 == 0) {
+        sum = sum + array[a];
+    }
+    Console.Write(array[a]);
+    a = a+1;
+    if (a < array.Length) {
+        Console.Write(",");
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Сумма всех элементов: " + sum);
