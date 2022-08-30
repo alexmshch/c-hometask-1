f188e26d42b18cdd5464e044f7a0e92d34ea6f04
alexey <alexm171002@mail.ru>
2022-08-30 20:20:11 +0300

    L6 is ready

---
+++ L6_(41)/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Укажите, сколько чисел Вы планируете вводить");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+int max = 0;
+
+void MorethanZero (int c) {
+    if(c > 0) {
+        max = max + 1;
+    }
+}
+
+if (a < 1) {
+    Console.WriteLine("Хорошего Вам дня, До свидания");
+} else {
+    while (b <= a) {
+        Console.WriteLine("Введите число "+b);
+        int c = Convert.ToInt32(Console.ReadLine());
+        MorethanZero(c);
+        b = b + 1;
+    }
+}
+Console.WriteLine(max);
