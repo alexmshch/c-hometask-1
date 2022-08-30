01249acffd351ebcb14de03017abb9b2aaee2d57
alexey <alexm171002@mail.ru>
2022-08-30 23:31:55 +0300

    L7 is ready 1

---
+++ L7_(47)/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double Randomizer() {
+    Random rand = new Random();
+    double number = rand.NextDouble();
+    int num = rand.Next(-100,100);
+    number = Math.Round(number*num,2);
+    return number;
+}
+
+double[,] table = new double[m,n];
+Random rand = new Random();
+for (int a = 0; a < m; a++) {
+    for (int b = 0; b < n; b++) {
+        table[a,b] = Randomizer();
+        Console.Write(table[a,b]+" ");
+    }
+    Console.WriteLine("");
+}
---
+++ L7_work/Program.cs
@@ -0,0 +1,27 @@
+﻿// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] table = new int [m, n];
+
+// for (int a = 0; a < m; a++){
+//     for (int b = 0; b < n; b++) {
+//         table[a,b] = a+b;
+//         Console.Write(table[a,b]);
+//     }
+//     Console.WriteLine("");
+// }
+
+// Random rand = new Random();
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [,] table = new int[m,n];
+
+// for (int i = 0; i < m; i++) {
+//     for (int j = 0; j < n; j++) {
+//         table[i,j] = rand.Next(-100,100);
+//         Console.Write(table[i,j]+" ");
+//     }
+//     Console.WriteLine("");
+// }
+
+
+//а остальные условия я не понял
